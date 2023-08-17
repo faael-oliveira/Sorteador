ff9072e0a5f53af09724af8332d3eda042196668
faael-oliveira <djfael.the.best@gmail.com>
2023-08-16 21:08:38 -0300

    Se o primeiro número for menor que o segundo pode prosseguir

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 // Função que gera números aleatórios
-function generateNumber(){
+function generateNumber() {
     // Mapeando o input do primeiro valor e fazendo o uso do Math.ceil
     const min = Math.ceil(document.querySelector(".input-min").value)
     // Mapeando o input do segundo valor e fazendo o uso do Math.floor
     const max = Math.floor(document.querySelector(".input-max").value)
-    // Gera um número inteiro aleatório dentro do intervalo [min, max]
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
-    // Exibe em o número gerado em um alerta na tela
-    alert(result)
+    // Se o primeiro número for menor que o segundo pode prosseguir
+    if (min < max) {
+        // Gera um número inteiro aleatório dentro do intervalo [min, max]
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        // Exibe em o número gerado em um alerta na tela
+        alert(result)
+    } else {
+        alert("O primeiro número precisa ser menor que o segundo")
+    }
+
 }
