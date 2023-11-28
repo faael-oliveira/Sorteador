f7cb546280a491f7df71a482d0f6b2f1a4e4669a
faael-oliveira <djfael.the.best@gmail.com>
2023-11-28 19:21:49 -0300

    Alteração na lógica

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function generateNumber() {
     // Mapeando o input do segundo valor e fazendo o uso do Math.floor
     const max = Math.floor(document.querySelector(".input-max").value)
     // Se o primeiro número for menor que o segundo pode prosseguir
-    if (min > max) {
+    if (min < max) {
         // Gera um número inteiro aleatório dentro do intervalo [min, max]
         const result = Math.floor(Math.random() * (max - min + 1) + min)
         // Exibe o número gerado em um alerta na tela
